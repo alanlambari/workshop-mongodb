f7a6a915309a03fd96ff8437b8853e4471a6533e
alanlambari <alanlambari@gmail.com>
2018-10-25 17:13:38 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/lambari/ajf/estudo/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.lambari.ajf.estudo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.lambari.ajf.estudo.workshopmongo.domain.Post;
+import br.com.lambari.ajf.estudo.workshopmongo.repository.PostRepository;
+import br.com.lambari.ajf.estudo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
